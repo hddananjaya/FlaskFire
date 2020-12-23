8e75c016e4483e393c2fffd6e7d50211a74b2634
Akila Dananjaya <akila.dananjaya@atlaslabs.com.au>
2020-12-23 12:58:15 +0530

    increased font size for emojis

--- src/static/js/utils/BBParser.js
+++ src/static/js/utils/BBParser.js
@@ -3,10 +3,18 @@ class BBParser {
   
   static parse(text) {
     const imgRe = /^\[img\](?<imgData>[/;,=+A-Za-z0-9:]*)\[\/img\]$/;
+    const urlRe = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    const exceptEmojisRe = /[A-Za-z0-9!@#$%^&*()_+=,./<?>{}{\]\[`~;:]/;
     const image = text.match(imgRe);
+    const url = text.match(urlRe);
+    const exceptEmojis = text.match(exceptEmojisRe);
     if (image) {
       const imageTag = `<img class="img-msg" src="${image.groups.imgData}">`;
       return imageTag;
+    } else if (url) {
+      return `<a target="_blank" href="${ url["0"] }">${ url["0"] }</a>`;
+    } else if (!exceptEmojis) {
+      return `<span style="font-size:30px"> ${ text } </span>`;
     } else {
       return text;
     }
